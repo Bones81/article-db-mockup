f0a62f17cf75642b5b17b9ca4415da6492c46d2c
Nathan Freeman <nathanlfreeman@gmail.com>
2022-10-16 14:25:29 -0400

    install and implement dotenv

--- test1/backend_article_db/server.js
+++ test1/backend_article_db/server.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3003
 
 
 app.get('/', (req, res) => {
     res.send('<h1>Express is connected!</h1>')
 })
 
-app.listen(3001, console.log('Express is listening...'));
+app.listen(PORT, () => {
+    console.log('Express is listening on PORT: ' + PORT);
+});
